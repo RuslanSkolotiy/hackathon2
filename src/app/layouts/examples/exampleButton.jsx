148398ab177ee095cd09a5d51100d1e765f28ae6
Olga Runova <ockaaa@gmail.com>
2021-10-09 14:42:24 +0300

    added button-module

--- src/app/layouts/examples/exampleButton.jsx
+++ src/app/layouts/examples/exampleButton.jsx
@@ -2,11 +2,21 @@ import React from "react"
 import Button from "../../components/ui/button/button"
 
 const ExampleButton = () => {
+  const clickHandler = (e) => {
+    console.log('click')
+  }
   return (
     <div className="mb-5">
       <h2>Button</h2>
       <div>
-        <Button />
+        <Button
+          type='button'
+          color='secondary'
+          outline={true}
+          handleClick={clickHandler}
+        >
+          Open
+        </Button>
       </div>
     </div>
   )
